e1742dfab6411396ded6fb0a8b93ef7ca52f2dd4
pzn62026 <39613497+pzn62026@users.noreply.github.com>
2018-06-01 17:37:19 +0800

    Update ballinbox.py

--- ball_in_box/ballinbox.py
+++ ball_in_box/ballinbox.py
@@ -4,29 +4,68 @@ from .validate import validate
 
 __all__ = ['ball_in_box']
 
-def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
+
+
+def pro_points(xrange, yrange, percision):
+
+    points = []
+    x_dv = (xrange[1] - xrange[0]) * 1.0 / percision
+    y_num = int((yrange[1] - yrange[0]) / x_dv)
+    for i in range(1, percision):
+        for j in range(1, y_num):
+            points.append((xrange[0] + i * x_dv, yrange[0] + j * x_dv))
+
+    return points
+
+def get_max_r(point, xrange, yrange, blockers, circles):
+
+    pre_r_list = []
+    pre_r_list.append(abs(point[0] - xrange[0]))
+    pre_r_list.append(abs(point[0] - xrange[1]))
+    pre_r_list.append(abs(point[1] - yrange[0]))
+    pre_r_list.append(abs(point[1] - yrange[1]))
+    for blocker in blockers:
+        d = math.sqrt((point[0] - blocker[0])**2 + (point[1] - blocker[1])**2)
+        pre_r_list.append(d)
+
+    for circle in circles:
+        d = math.sqrt((point[0] - circle[0])**2 +
+                      (point[1] - circle[1])**2) - circle[2]
+        
+        if d <= 0:
+            return 0
+        pre_r_list.append(d)
+    r = xrange[1] - xrange[0]
+    for pre_r in pre_r_list:
+        if pre_r < r:
+            r = pre_r
+    return r
+
+
+def ball_in_box(m, blockers):
     """
     m is the number circles.
     n is the list of coordinates of tiny blocks.
     
     This returns a list of tuple, composed of x,y of the circle and r of the circle.
     """
-
-    # The following is an example implementation.
+    xrange = [-1,1]
+    yrange = [-1,1]
+    percision = 800
     circles = []
-    for circle_index in range(m):
+    points = pro_points(xrange, yrange, percision)
+    for i in range(1,m):
+        temp_r = 0
+        circle = [0, 0, 0]
+        for point in points:
+            r = get_max_r(point, xrange, yrange, blockers, circles)
+            if r > temp_r:
+                temp_r = r
+                circle[0] = point[0]
+                circle[1] = point[1]
+                circle[2] = temp_r
 
-        x = random.random()*2 - 1
-        y = random.random()*2 - 1
-        r = random.random()*0.1
-
-        circles.append((x, y, r))
-        while not validate(circles, blockers):
-            x = random.random()*2 - 1
-            y = random.random()*2 - 1
-            r = random.random()*0.1
-            circles[circle_index] = (x, y, r)
-
-        circle_index += 1
-    
+        points.remove((circle[0], circle[1]))
+        circles.append((circle[0], circle[1], circle[2]))
+  
     return circles
